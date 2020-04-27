c2d560c8d5f5f814ae3c9ffab69d197e2cc2b8a5
Maximilian Muecke <muecke.maximilian@gmail.com>
2020-04-27 09:19:01 +0200

    Update build.py

--- text-normalizer/build.py
+++ text-normalizer/build.py
@@ -1,1 +1,27 @@
+def identity(words):
+    """Identity function for tokenizer param in CountVectorizer."""
+    return words
 
+def create_pipeline(estimator=None):
+    """Create sklearn Pipeline object."""
+    steps = [
+        ('norm', TextNormalizer()),
+        ('vect', CountVectorizer(
+            tokenizer=identity, lowercase=False
+        )),
+        ('tfidf', TfidfTransformer())
+    ]
+    # return w/o estimator
+    if not estimator:
+        return Pipeline(steps)
+    # add the estimator
+    steps.append(('clf', estimator))
+    return Pipeline(steps)
+    
+def run_multiclass():
+    """Add multiclass estimators to Pipeline."""
+    model_list = []
+    for form in (MultinomialNB, GaussianNB, 
+                 LogisticRegression, SGDClassifier):
+        model_list.append(create_pipeline(form()))
+    return model_list
