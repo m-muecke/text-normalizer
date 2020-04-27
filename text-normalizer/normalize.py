caa09714077483e374189e0a31ce9bd9feace05d
Maximilian Muecke <muecke.maximilian@gmail.com>
2020-04-27 09:18:47 +0200

    Update normalize.py

--- text-normalizer/normalize.py
+++ text-normalizer/normalize.py
@@ -112,34 +112,6 @@ class TextNormalizerNLTK(TransformerMixin, BaseEstimator):
                                  remove_stopwords=self.remove_stopwords,
                                  apply_lemmatize=self.apply_lemmatize)
 
-def identity(words):
-    """Identity function for tokenizer param in CountVectorizer."""
-    return words
-
-def create_pipeline(estimator=None):
-    """Create sklearn Pipeline object."""
-    steps = [
-        ('norm', TextNormalizer()),
-        ('vect', CountVectorizer(
-            tokenizer=identity, lowercase=False
-        )),
-        ('tfidf', TfidfTransformer())
-    ]
-    # return w/o estimator
-    if not estimator:
-        return Pipeline(steps)
-    # add the estimator
-    steps.append(('clf', estimator))
-    return Pipeline(steps)
-    
-def run_multiclass():
-    """Add multiclass estimators to Pipeline."""
-    model_list = []
-    for form in (MultinomialNB, GaussianNB, 
-                 LogisticRegression, SGDClassifier):
-        model_list.append(create_pipeline(form()))
-    return model_list
-
 if __name__ == '__main__':
     # TODO: add ML functionalities
     # TODO: add docstrings
